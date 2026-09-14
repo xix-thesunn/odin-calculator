b4c82e9da0ab411e8fde062c8e65eea6d1a2f051
xix-thesunn <131435522+xix-thesunn@users.noreply.github.com>
2026-09-14 17:53:47 +0300

    Make working number and deletion buttons

--- calculator.js
+++ calculator.js
@@ -1,17 +1,21 @@
-const buttons = document.querySelectorAll('button');
+const buttonsList = document.querySelectorAll('button');
+const buttons = Array.from(buttonsList);
+const display = document.querySelector('#display');
+display.textContent = '0';
 
+
+// VIBRATION TEST
 for (button of buttons) {
     button.addEventListener('click', () => {
-  // Check if browser supports the API
   if ('vibrate' in navigator) {
-    navigator.vibrate(40); // Short 40ms tap
+    navigator.vibrate(40);
   }
 });
 }
 
 for (button of buttons) {
   if (button.classList.contains('numbers')) {
-    button.addEventListener('click', (e) => testAlert(e));
+    button.addEventListener('click', e => enterTheNumber(e.target));
   }
 
   if (button.classList.contains('operator')) {
@@ -23,15 +27,35 @@ for (button of buttons) {
   }
 
   if (button.getAttribute('id') === 'clear') {
-    button.addEventListener('click', (e) => testAlert(e));
+    button.addEventListener('click', () => clearDisplay());
   }
 
   if (button.getAttribute('id') === 'backspace') {
-    button.addEventListener('click', (e) => testAlert(e));
+    button.addEventListener('click', () => backspaceFunc());
   }
 }
 
 function testAlert(e) {
   let button = e.target;
   alert('You clicked button ' + button.getAttribute('id'));
+}
+
+function enterTheNumber(button) {
+  if (display.textContent === '0') {
+    display.textContent = button.textContent;
+  } else if (display.textContent.length < 8) {
+  display.textContent += button.textContent;
+  }
+}
+
+function clearDisplay() {
+  display.textContent = '0';
+}
+
+function backspaceFunc() {
+  if (display.textContent.length <= 1) {
+    display.textContent = '0';
+  } else {
+  display.textContent = display.textContent.slice(0, -1);
+  }
 }
