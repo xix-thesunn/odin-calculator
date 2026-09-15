9ed94a891966a9c443072894c758a4eef3c817a2
XIX The Sunn <131435522+xix-thesunn@users.noreply.github.com>
2026-09-15 22:56:49 +0300

    Add computing logic + physical keyboard support

--- calculator.js
+++ calculator.js
@@ -12,7 +12,7 @@ const history = document.querySelector('.history');
 // After pressing '=' whole expession will be computed.
 let expression = [];
 let readyToNewNumber = false;
-let result = 0;
+let resultOfExpression = 0;
 
 
 // HAPTIC EFFECT
@@ -24,6 +24,52 @@ for (button of buttons) {
 });
 }
 
+// BASIC COMPUTING LOGIC
+
+// It's kinda complicated bit it allows compute
+// our expression in right priority (multiply first,
+// divide second, etc)
+function applyMultiply(expForComputing) {
+  while (expForComputing.indexOf('×') != -1) {
+    let index = expForComputing.indexOf('×');
+    let result = expForComputing[index - 1] * expForComputing[index + 1];
+    expForComputing.splice(index - 1, 3, result);
+  };
+  return expForComputing;
+};
+  
+
+
+function applyDivide(expForComputing) {
+  while (expForComputing.indexOf('÷') != -1) {
+    let index = expForComputing.indexOf('÷');
+    if (expForComputing[index + 1] === 0) { expForComputing.push('ERROR') } 
+    let result = expForComputing[index - 1] / expForComputing[index + 1];
+    expForComputing.splice(index - 1, 3, result);
+  };
+  return expForComputing;
+};
+
+function applyPlus(expForComputing) {
+  while (expForComputing.indexOf('+') != -1) {
+    let index = expForComputing.indexOf('+');
+    let result = expForComputing[index - 1] + expForComputing[index + 1];
+    expForComputing.splice(index - 1, 3, result);
+  };
+  return expForComputing;
+};
+
+function applyMinus(expForComputing) {
+  while (expForComputing.indexOf('-') != -1) {
+    let index = expForComputing.indexOf('-');
+    let result = expForComputing[index - 1] - expForComputing[index + 1];
+    expForComputing.splice(index - 1, 3, result);
+  };
+  return expForComputing;
+};
+
+
+// WORKING BUTTONS
 for (button of buttons) {
   if (button.classList.contains('numbers')) {
     button.addEventListener('click', e => enterTheNumber(e.target));
@@ -50,7 +96,69 @@ for (button of buttons) {
   }
 }
 
+// SAME BUT FOR PHYSICAL KEYBOARD
+document.addEventListener('keydown', e => {
+  switch (e.key) {
+    case '0':
+      buttonsList[0].click();
+      break;
+    case '1':
+      buttonsList[1].click();
+      break;
+    case '2':
+      buttonsList[2].click();
+      break;
+    case '3':
+      buttonsList[3].click();
+      break;
+    case '4':
+      buttonsList[4].click();
+      break;
+    case '5':
+      buttonsList[5].click();
+      break;
+    case '6':
+      buttonsList[6].click();
+      break;
+    case '7':
+      buttonsList[7].click();
+      break;
+    case '8':
+      buttonsList[8].click();
+      break;
+    case '9':
+      buttonsList[9].click();
+      break;
+    case '.':
+      buttonsList[10].click();
+      break;
+    case '*':
+      buttonsList[11].click();
+      break;
+    case '/':
+      buttonsList[12].click();
+      break;
+    case '-':
+      buttonsList[13].click();
+      break;
+    case '+':
+      buttonsList[14].click();
+      break;
+    case 'Enter':
+      buttonsList[15].click();
+      break;
+    case 'Escape':
+      buttonsList[16].click();
+      break;
+    case 'Backspace':
+      buttonsList[17].click();
+      break;
+    default:
+      break;
+  }
+});
 
+// HERE IS THE LOGIC FOR CALCULATOR WORKING WELL
 function enterTheNumber(button) {
   if (display.textContent === '0' || readyToNewNumber === true) {
     display.textContent = button.textContent;
@@ -64,6 +172,7 @@ function enterTheNumber(button) {
 function addPoint() {
   if (display.textContent.indexOf('.') === -1 && display.textContent.length < 8) {
     display.textContent += '.';
+    readyToNewNumber = false;
   }
 };
 
@@ -76,7 +185,7 @@ function clearAll() {
 };
 
 function backspaceFunc() {
-  if (display.textContent.length <= 1) {
+  if (display.textContent.length <= 1 || display.textContent.length <= 2 && Number(display.textContent) < 0) {
     display.textContent = '0';
   } else {
   display.textContent = display.textContent.slice(0, -1);
@@ -96,27 +205,61 @@ function operatorPressed(activeButton) {
 };
 
 function pressEqual() {
-  if (expression.length >= 1) {
-    if (readyToNewNumber === true) {
-      expression.pop();
-    } else {
-      expression.push(Number(display.textContent));
-    };
+  if (readyToNewNumber === true && expression.length >= 3) {
+    expression.pop();
     for (button of operatorButtons) {
       button.classList.remove('pressed');
     };
-    compute();
-  }
+    computeAndReset()
+  }; 
+  if (readyToNewNumber === false && expression.length >= 2) {
+    expression.push(Number(display.textContent));
+    for (button of operatorButtons) {
+      button.classList.remove('pressed');
+    };
+    computeAndReset();
+  };
+};
+
+function computeAndReset() {
+  let expForComputing = [...expression];
+
+  expForComputing = applyMultiply(expForComputing);
+  expForComputing = applyDivide(expForComputing);
+  expForComputing = applyMinus(expForComputing);
+  expForComputing = applyPlus(expForComputing);
+
+  if (expForComputing.length === 1) { 
+    resultOfExpression = expForComputing[0];
+    if (getDecimalLength(resultOfExpression) > 7) {
+      resultOfExpression = Number(resultOfExpression.toFixed(7));
+    };
+  } else {
+    resultOfExpression = 'Error';
+  };
+
+  expression.push('=', resultOfExpression);
+  if (typeof resultOfExpression === 'number') { writeInLog() };
+  readyToNewNumber = true;
+  display.textContent = resultOfExpression;
+  expression = [];
 }
 
-function compute() {
-  let expForComputing = [...expression];
-  
-
-
-  expression.push('=', result);
+function writeInLog() {
   let expessionLog = document.createElement('p');
   expessionLog.textContent = expression.join(' ');
+  if (history.childElementCount >= 8) {
+    history.firstElementChild.remove();
+  }
   history.appendChild(expessionLog);  
-  clearAll();
+  console.log(expessionLog.textContent);
 }
+
+function getDecimalLength(number) {
+  let str = number.toString();
+  if (!str.includes('.')) return 0;
+  return str.split('.')[1].length;
+}
+
+
+
