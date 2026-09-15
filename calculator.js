5dd1666541cbf04583b2dd879e7b430903700932
xix-thesunn <131435522+xix-thesunn@users.noreply.github.com>
2026-09-15 14:02:14 +0300

    Add functionality for operator buttons and equal + working history

--- calculator.js
+++ calculator.js
@@ -1,15 +1,28 @@
 const buttonsList = document.querySelectorAll('button');
 const buttons = Array.from(buttonsList);
+const operatorButtons = buttons.filter(button => button.classList.contains('operator'));
+
 const display = document.querySelector('#display');
 display.textContent = '0';
 
+const history = document.querySelector('.history');
 
-// VIBRATION TEST
+// Here we will store numbers and operators, and it will
+// look like: '2', '+', '2', '×', '3'.
+// After pressing '=' whole expession will be computed.
+let expression = [];
+let readyToNewNumber = false;
+let result = 0;
+
+
+// HAPTIC EFFECT
 for (button of buttons) {
     button.addEventListener('click', () => {
   if ('vibrate' in navigator) {
     navigator.vibrate(40);
   }
+
+  console.log(expression.join(''))
 });
 }
 
@@ -18,16 +31,20 @@ for (button of buttons) {
     button.addEventListener('click', e => enterTheNumber(e.target));
   }
 
+  if (button.getAttribute('id') === 'point') {
+    button.addEventListener('click', () => addPoint());
+  }
+
   if (button.classList.contains('operator')) {
-    button.addEventListener('click', (e) => testAlert(e));
+    button.addEventListener('click', e => operatorPressed(e.target));
   }
 
   if (button.getAttribute('id') === 'equal') {
-    button.addEventListener('click', (e) => testAlert(e));
+    button.addEventListener('click', () => pressEqual());
   }
 
   if (button.getAttribute('id') === 'clear') {
-    button.addEventListener('click', () => clearDisplay());
+    button.addEventListener('click', () => clearAll());
   }
 
   if (button.getAttribute('id') === 'backspace') {
@@ -35,22 +52,30 @@ for (button of buttons) {
   }
 }
 
-function testAlert(e) {
-  let button = e.target;
-  alert('You clicked button ' + button.getAttribute('id'));
-}
 
 function enterTheNumber(button) {
-  if (display.textContent === '0') {
+  if (display.textContent === '0' || readyToNewNumber === true) {
     display.textContent = button.textContent;
-  } else if (display.textContent.length < 8) {
+    readyToNewNumber = false;
+  } else if ((display.textContent.length < 8 && display.textContent.indexOf('.') === -1)
+          || (display.textContent.length < 9 && display.textContent.indexOf('.') != -1)) {
   display.textContent += button.textContent;
   }
-}
+};
 
-function clearDisplay() {
+function addPoint() {
+  if (display.textContent.indexOf('.') === -1 && display.textContent.length < 8) {
+    display.textContent += '.';
+  }
+};
+
+function clearAll() {
+  expression = [];
   display.textContent = '0';
-}
+  for (button of operatorButtons) {
+    button.classList.remove('pressed');
+  };
+};
 
 function backspaceFunc() {
   if (display.textContent.length <= 1) {
@@ -58,4 +83,42 @@ function backspaceFunc() {
   } else {
   display.textContent = display.textContent.slice(0, -1);
   }
+};
+
+function operatorPressed(activeButton) {
+  for (button of operatorButtons) {
+    button.classList.remove('pressed');
+  };
+  activeButton.classList.add('pressed');
+
+  expression.push(Number(display.textContent));
+  expression.push(activeButton.textContent);
+
+  readyToNewNumber = true;
+};
+
+function pressEqual() {
+  if (expression.length >= 1) {
+    if (readyToNewNumber === true) {
+      expression.pop();
+    } else {
+      expression.push(Number(display.textContent));
+    };
+    for (button of operatorButtons) {
+      button.classList.remove('pressed');
+    };
+    compute();
+  }
+}
+
+function compute() {
+  let expForComputing = [...expression];
+  
+
+
+  expression.push('=', result);
+  let expessionLog = document.createElement('p');
+  expessionLog.textContent = expression.join(' ');
+  history.appendChild(expessionLog);  
+  clearAll();
 }
